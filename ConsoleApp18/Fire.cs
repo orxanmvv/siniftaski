dd4cfa09fbd99b8a7d266d123f82e1d8b87feddb
orxanmvv <m.orxan.m123@gmail.com>
2024-04-02 09:31:09 +0400

    siniftaski commiti

---
+++ ConsoleApp18/Fire.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Fire
+    {
+        public int Capacitybullet;
+        public int Currentbullet;
+        public bool Firemode;
+        public int Capacity2;
+
+        
+        
+        public Fire(int capacityBullet, int currentBullet, bool fireMode)
+        {
+            Capacitybullet = capacityBullet;
+            Currentbullet = currentBullet;
+            Firemode = fireMode;
+        }
+
+        public void Shoot()
+        {
+            if (Currentbullet > 0)
+            {
+                if (Firemode=true) 
+                {
+                    Currentbullet = 0;
+                    Console.WriteLine("butun gulleler ateslendi");
+
+                }
+                else 
+                {
+                   
+                    Currentbullet--;
+                    Console.WriteLine("tek gulle atildi");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Gulle yoxdur!");
+            }
+        }
+
+
+        public void Getremainbulletcount() {
+
+
+            Console.WriteLine("daraq ucun lazim olan gulle sayi");
+            Console.WriteLine( Capacitybullet - Currentbullet);
+        
+        
+        }
+        public void Reload ( )
+        {
+            Capacitybullet = Currentbullet;
+            Console.WriteLine("daraq reload oldu");
+
+
+        }
+        public void Changefirmode()
+        {
+            if (Firemode = true)
+            {
+                Firemode = false;
+            }
+            else {Firemode = true;}
+        }
+        public void Showfullinfo()
+        {
+            Console.WriteLine($"Daragin gulle tutumu:{Capacitybullet},Daragdaki gulle sayi:{Currentbullet}");
+        }
+
+
+
+    }
+   
+
+
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false; do {
+
+                Fire fire = new Fire(80, 40, true);
+                Console.WriteLine("0-Informasiya almaq ucun");
+                Console.WriteLine("1-Ates acmaq ucun");
+                Console.WriteLine("2-Getremainbullet ucun");
+                Console.WriteLine("3-Reload ucun");
+                Console.WriteLine("4-Changefiremode ucun");
+                Console.WriteLine("5-Programi bitir");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        fire.Showfullinfo();
+                        break;
+                    case "1":
+                        fire.Shoot();
+                        break;
+                    case "2":
+                        fire.Getremainbulletcount();
+                        break;
+                    case "3":
+                        fire.Reload();
+                        break;
+                    case "4":
+                        fire.Changefirmode();
+                        break;
+                    case "5": exit = true;
+                        Console.WriteLine("program bitdi");
+                        break;
+                    default:
+                        Console.WriteLine("duzgun secim edin");
+                        break;
+
+
+
+                }
+              
+
+            } while (exit!=true);
+          
+          
+        }
+    }
+}
